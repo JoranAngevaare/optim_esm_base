7613f24978c38f36634b9841de6faa932760767d
Joran R. Angevaare <joranangevaare@gmail.com>
2023-05-16 08:36:39 +0200

    Conda env from requirements file

--- create_env.sh
+++ create_env.sh
@@ -1,12 +1,24 @@
-env_name="${1:-"optimesm"}"
-synda_dir_name="${2:-"($pwd)/synda"}"
-py_version='3.8.16'
+synda_dir_name="${1:-"$(pwd)/synda"}"
 
+function announce {
+    echo
+    echo "#######################################################################################"
+    echo "## $1       ("`date -u`")"
+    echo
+}
 
-echo "Start install, conda create $env_name, python=$py_version and set synda dir to $synda_dir_name"
+announce "Start install on $(which python), and set synda dir to $synda_dir_name"
 mkdir $synda_dir_name
 
-echo "start building env"
-conda create -n $env_name python=$py_version --yes
+conda config --add channels conda-forge
+conda config --add channels IPSL
+conda config --set channel_priority flexible
+#from_conda_forge="myproxy==6.2.6 cartopy==0.20.2 xesmf==0.6.3 cdo==2.0.3"
+#announce "install from confa forge: $from_conda_forge"
+conda install --file conda_requirements.txt --yes -q
 
-conda activate $env_name
+announce "install requirements"
+pip install -r requirements.txt
+
+announce "set ST_HOME"
+# conda env config vars set ST_HOME=$synda_dir_name
