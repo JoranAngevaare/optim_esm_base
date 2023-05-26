b7ee60218b762ac3392008724b8360d1feb8abee
Joran Angevaare <joranangevaare@gmail.com>
2023-05-26 12:19:51 +0200

    fix argparsing

--- install_software.sh
+++ install_software.sh
@@ -3,31 +3,27 @@ POSITIONAL_ARGS=()
 
 # Thanks https://stackoverflow.com/a/14203146/18280620
 while [[ $# -gt 0 ]]; do
-  case $1 in
-    --no_synda)
-      no_synda=1
-      shift # past argument
-      shift # past value
-      ;;
-    --no_cdo)
-      no_cdo=1
-      shift # past argument
-      shift # past value
-      ;;
-    --synda_dir)
-      synda_dir="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    -*|--*)
-      echo "Unknown option $1"
-      exit 1
-      ;;
-    *)
-      POSITIONAL_ARGS+=("$1") # save positional arg
-      shift # past argument
-      ;;
-  esac
+    case "$1" in
+        --no_synda)
+            no_synda=1
+            shift
+        ;;
+        --no_cdo)
+            no_cdo=1
+            shift
+        ;;
+        --synda_dir)
+            synda_dir="$2"
+            shift 2
+        ;;
+        -*|--*)
+            echo "Unknown option $1"
+            exit 1
+        ;;
+        *)
+            break
+        ;;
+    esac
 done
 
 set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
@@ -45,7 +41,7 @@ function announce {
     echo
 }
 
-if [[ $no_synda == 1 ]]; 
+if [[ $no_synda == 1 ]];
 then
     echo "Skip SYNDA install"
     conda env config vars set BASE_SYNDA_CDO=1
@@ -57,16 +53,16 @@ else
     else
         mkdir $synda_dir_name
     fi
-
+    
     synda_v="synda==3.35"
     announce "install $synda_v from IPSL"
     conda install -c IPSL $synda_v  --yes -q
-
+    
     announce "set ST_HOME"
     conda env config vars set ST_HOME=$synda_dir_name
 fi
 
-if [[ $no_cdo == 1 ]]; 
+if [[ $no_cdo == 1 ]];
 then
     echo "Skip CDO install"
     announce "install from confa forge:\n$(cat conda_requirements.txt | grep -v cdo)"
@@ -81,5 +77,3 @@ fi
 
 announce "install requirements"
 pip install -r requirements.txt
-
-
