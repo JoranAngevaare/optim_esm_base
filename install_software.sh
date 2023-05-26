779775413a0afeb1fddd8c6d26c0ec36a9fc08d8
Joran R. Angevaare <joranangevaare@gmail.com>
2023-05-26 14:10:43 +0200

    Use mamba over conda (#50)
    * Use mamba over conda
    
    * mamba
    
    * Update test_create_env.yml
    
    * Update test_create_env.yml

--- install_software.sh
+++ install_software.sh
@@ -44,7 +44,7 @@ function announce {
 if [[ $no_synda == 1 ]];
 then
     echo "Skip SYNDA install"
-    conda env config vars set BASE_SYNDA_CDO=1
+    mamba env config vars set BASE_SYNDA_CDO=1
 else
     announce "Start install on $(which python), and set synda dir to $synda_dir_name"
     if [[ -d "$synda_dir_name" ]]
@@ -56,10 +56,10 @@ else
     
     synda_v="synda==3.35"
     announce "install $synda_v from IPSL"
-    conda install -c IPSL $synda_v  --yes -q
+    mamba install -c IPSL $synda_v  --yes -q
     
     announce "set ST_HOME"
-    conda env config vars set ST_HOME=$synda_dir_name
+    mamba env config vars set ST_HOME=$synda_dir_name
 fi
 
 if [[ $no_cdo == 1 ]];
@@ -67,12 +67,12 @@ then
     echo "Skip CDO install"
     announce "install from confa forge:\n$(cat conda_requirements.txt | grep -v cdo)"
     cat conda_requirements.txt | grep -v cdo >> .tmp.txt
-    conda install -c conda-forge --file .tmp.txt --yes -q
+    mamba install -c conda-forge --file .tmp.txt --yes -q
     rm .tmp.txt
-    conda env config vars set BASE_NO_CDO=1
+    mamba env config vars set BASE_NO_CDO=1
 else
     announce "install from confa forge:\n$(cat conda_requirements.txt)"
-    conda install -c conda-forge --file conda_requirements.txt --yes -q
+    mamba install -c conda-forge --file conda_requirements.txt --yes -q
 fi
 
 announce "install requirements"
