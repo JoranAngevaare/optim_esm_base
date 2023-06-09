861e7066beb521a2e652154800f93b739f4f495e
JoranAngevaare <joranangevaare@gmail.com>
2023-06-09 09:49:23 +0200

    allow mamba and conda

--- install_software.sh
+++ install_software.sh
@@ -4,6 +4,9 @@ POSITIONAL_ARGS=()
 # Thanks https://stackoverflow.com/a/14203146/18280620
 while [[ $# -gt 0 ]]; do
     case "$1" in
+        --installer)
+            installer="$2"
+            shift 2
         --no_synda)
             no_synda=1
             shift
@@ -33,9 +36,10 @@ done
 set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 
 
+installer="${installer:"mamba"}"
 synda_dir_name="${synda_dir:-"$(pwd)/synda"}"
 
-echo "args are" synda_dir:$synda_dir_name no_synda:$no_synda no_cdo:$no_cdo
+echo "args are" synda_dir:$synda_dir_name no_synda:$no_synda no_cdo:$no_cdo installer:$installer
 
 function announce {
     echo
@@ -48,7 +52,7 @@ function announce {
 if [[ $no_synda == 1 ]];
 then
     echo "Skip SYNDA install"
-    mamba env config vars set BASE_SYNDA_CDO=1
+    $installer env config vars set BASE_SYNDA_CDO=1
 else
     announce "Start install on $(which python), and set synda dir to $synda_dir_name"
     if [[ -d "$synda_dir_name" ]]
@@ -60,10 +64,10 @@ else
     
     synda_v="synda==3.35"
     announce "install $synda_v from IPSL"
-    mamba install -c IPSL $synda_v  --yes -q
+    $installer install -c IPSL $synda_v  --yes -q
     
     announce "set ST_HOME"
-    mamba env config vars set ST_HOME=$synda_dir_name
+    $installer env config vars set ST_HOME=$synda_dir_name
 fi
 
 if [[ $only_synda == 1 ]];
@@ -76,7 +80,7 @@ if [[ $no_cdo == 1 ]];
 then
     echo "Skip CDO install"
     cat conda_requirements.txt | grep -v cdo >> tmp.txt
-    mamba env config vars set BASE_NO_CDO=1
+    $installer env config vars set BASE_NO_CDO=1
 else
     cat conda_requirements.txt | grep -v "somethingsomething" >> tmp.txt
 fi
@@ -87,7 +91,7 @@ announce "install from conda forge:\n$(cat tmp.txt)"
 for dep in $(cat tmp.txt);
 do
     announce "install $dep";
-    mamba install -c conda-forge "$dep" --yes;
+    $installer install -c conda-forge "$dep" --yes;
 done
 rm tmp.txt
 
